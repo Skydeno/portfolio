0045bd5d6e41cb584c7593ba778fd9a3e486b2ab
Konrad Pietralski <konrad.pietralski@gmail.com>
2024-11-28 12:52:20 +0100

    New CV and updated experience

--- components/home/about/About.tsx
+++ components/home/about/About.tsx
@@ -13,31 +13,30 @@ export const About = () => {
         <div>
           <Reveal>
             <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
-              Hey! I&apos;m Konrad, if you didn&apos;t already know. I&apos;m a
-              web developer from Gdańsk, Poland. I specialize in the frontend,
-              primarily React.js and Typescript, but love building with whatever
-              tools are right for the job.
+              Hey! I&apos;m Konrad. I&apos;m a web developer from Gdańsk,
+              Poland. I specialize in the frontend, primarily React.js and
+              Typescript, but love building with whatever tools are right for
+              the job.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              I worked for a company named WizCode for a year. I&apos;m also
+              I worked for a company named WizCode for a year. I&apos;m was also
               doing projects for companies as freelancer.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              Outside of work, I love aviation. Any time there is nice weather
-              you&apos;ll find me spotting planes in Gdańsk airport. If
-              it&apos;s ☁️ i spend my free time increasing my developer skills
-              and play flight simulator.
+              I love aviation. Any time there is nice weather you&apos;ll find
+              me spotting planes in Gdańsk airport. If it&apos;s ☁️ i spend my
+              free time increasing my developer skills and play flight
+              simulator.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              I&apos;m actively looking for new position where I can merge my
-              love for code with my love for aviation. If you think you&apos;ve
-              got an opening that I might like, let&apos;s connect 🔗
+              I&apos;m looking for new position where I can merge my love for
+              code with my love for aviation.
             </p>
           </Reveal>
           <Reveal>
